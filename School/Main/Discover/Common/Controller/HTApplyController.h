6e54c90c54e571371063faff91ddb976ea50d2b7
Charles <2485539514@qq.com>
2017-12-19 18:28:03 +0800

    发现

---
+++ School/Main/Discover/Common/Controller/HTApplyController.h
@@ -0,0 +1,13 @@
+//
+//  HTApplyController.h
+//  School
+//
+//  Created by Charles Cao on 2017/12/19.
+//  Copyright © 2017年 hublot. All rights reserved.
+//
+
+#import "HTPageController.h"
+
+@interface HTApplyController : HTPageController
+
+@end
